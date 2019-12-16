967df381d2682cd19a0368f90f22964999135b3b
Jamsheed Mistri <jmistri7@gmail.com>
2019-12-15 19:51:00 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Berkeley Purity Test</title>
+	<style>
+		body, html {
+			background: #ffe699;
+		}
+
+		p {
+			font-size: 1.25em;
+		}
+	</style>
+</head>
+<body>
+	<center>
+		<h1>Berkeley Purity Test</h1>
+		<p>Coming soon. How'd you even find this page?</p>
+		<p>Well, since you're here, if you have suggestions for questions we should add to the Berkeley Purity Test, send them over to <a href="mailto:contact@berkeleypuritytest.com">contact@berkeleypuritytest.com</a>.</p>
+	</center>
+</body>
+</html>
